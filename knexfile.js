1a24d2673e5550580dae91e12b9a4385c3eefee9
alanblee <alanbenlee12@gmail.com>
2020-05-18 12:42:46 -0700

    intial setup

---
+++ knexfile.js
@@ -0,0 +1,33 @@
+module.exports = {
+  development: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./data/migrations",
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./data/migrations",
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+};
